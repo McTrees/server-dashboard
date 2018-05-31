e9c9bd1fae711614e95ae5c9cc19f7acfc5b9f54
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-31 20:07:48 +0100

    Add `addUser` function

--- db_fns.js
+++ db_fns.js
@@ -1,3 +1,9 @@
 //This file will have high-level functions for the DB
 var sqlite3 = require("sqlite3")
 let db = new sqlite3.Database('users.db');
+
+
+
+exports.addUser = function(username, hash) { //This function adds a user to the database
+  db.run("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+}
