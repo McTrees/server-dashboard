0e3472770f25ada68ef93e3afb8d675858d184bb
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-31 20:34:30 +0100

    export this

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ var fns = require("./db_fns.js")
 let db = new sqlite3.Database('users.db');
 
 
-function createUser(username, password) { // This function creates an account with the specified username and password
+exports.createUser = function(username, password) { // This function creates an account with the specified username and password
   let hash = bcrypt.hashSync(password, 10);
   fns.addUser(username, hash)
 }
