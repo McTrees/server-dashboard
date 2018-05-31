ab7a24da6bd1e72df0902482c9ddb98c6413320c
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-31 20:14:48 +0100

    Finish addUser

--- auth.js
+++ auth.js
@@ -1,8 +1,11 @@
 //This file will handle authentication
 var sqlite3 = require("sqlite3")
+var bcrypt = require('bcrypt')
 var fns = require("db_fns.js")
 let db = new sqlite3.Database('users.db');
 
+
 function createUser(username, password) { // This function creates an account with the specified username and password
-  
+  let hash = bcrypt.hashSync(password, 10);
+  fns.addUser(username, hash)
 }
