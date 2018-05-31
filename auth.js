a24e48ab793d42259d3b2802e46805fe76310c3c
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-31 20:08:02 +0100

    start stub functions

--- auth.js
+++ auth.js
@@ -1,1 +1,8 @@
 //This file will handle authentication
+var sqlite3 = require("sqlite3")
+var fns = require("db_fns.js")
+let db = new sqlite3.Database('users.db');
+
+function createUser(username, password) { // This function creates an account with the specified username and password
+  
+}
